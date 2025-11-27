3c763ccf75e9a3f05cd70aa61f29a31b759edc75
eldezaki <152847114+eldezaki@users.noreply.github.com>
2025-11-27 10:08:55 +0700

    first commit

---
+++ Pertemuan14/Kafe28.java
@@ -0,0 +1,19 @@
+package Pertemuan14;
+public class Kafe28 {
+    public static void main(String[] args) {
+        Menu();
+    }
+
+    public static void Menu(){
+        System.out.println("=== MENU RESTO KAFE ===");
+        System.out.println("1. Kopi Hitam - Rp 15,000");
+        System.out.println("2. Cappuccino - Rp 25,000");
+        System.out.println("3. Latte - Rp 30,000");
+        System.out.println("4. Teh Tarik - Rp 12,000");
+        System.out.println("5. Roti Bakar - Rp 10,000");
+        System.out.println("6. Mie Goreng - Rp 18,000");
+        System.out.println("=========================");
+        System.out.println("4. Espresso - Rp 20,000");
+    }
+}
+8
